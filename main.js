25bbcddd88aab832550134f2cea496415ca3be88
Boyan <104321637+BoyanDashev@users.noreply.github.com>
2023-12-08 18:25:53 +0200

    Part 5 exercise 2

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 // array.reduce(function (total, currentValue, currentIndex, arr), initialValue)
 //
 // Каква трябв ада бъде функцията: Трябв ада взима currentvalue и да го конкатва към следващо докато не края в един array
+
+//console.log(
+  //arrays.reduce(function (flat, current) {
+    //return flat.concat(current);
+  //}, [])
+//);
 let concatArray = arrays.reduce(ConcatArray);
 
 function ConcatArray(arraistogether, differentarrays) {
